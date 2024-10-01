e8fd11621a34411f17368812b085d544b4fc313c
FranGuam <frankguansy@hotmail.com>
2024-10-01 14:39:04 +0000

    feat: HW1 solution

---
+++ HW1/README.md
@@ -0,0 +1,9 @@
+# 环境配置
+
+```bash
+rosdep install --from-paths . --ignore-src -r -y
+```
+
+```bash
+apt install ros-noetic-ros-controllers ros-noetic-ackermann-msgs
+```
--- HW1/catkin_sim/src/akm_sim/racebot_control/script/cross_demo.py
+++ HW1/catkin_sim/src/akm_sim/racebot_control/script/cross_demo.py
@@ -5,6 +5,8 @@ import rospy
 import signal
 from geometry_msgs.msg import Twist, Pose
 from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+import math
 
 class CrossDemo:
     def __init__(self, robot_name):
@@ -12,19 +14,59 @@ class CrossDemo:
         self.robot_name = robot_name
         self.cmd_twist = Twist()
         self.pose = Pose()
+        self.direction = 0
         self.cmd_pub = rospy.Publisher(self.robot_name+'/cmd_vel', Twist, queue_size=1)
         self.odom_sub = rospy.Subscriber(self.robot_name+'/odom', Odometry, self.odom_callback)
         self.Main()
-        rospy.spin()
+        exit()
     
     def Main(self):
-        while self.pose.position.y < 3:
-            print("x: " + str(self.pose.position.x) + " y: " + str(self.pose.position.y))
+        self.turn(4.25, 2.95, "right")
+        rospy.loginfo("Turn 1 completed!")
+        while not self.is_in_target_area(3.5, 5, 2.5, 3):
             self.CarMove(1, 0)
-            rospy.sleep(0.5)
+            rospy.sleep(0.05)
+            self.process_odom()
         self.CarMove(0, 0)
+        rospy.loginfo("Straight 1 completed!")
+        self.turn(1, 14.5, "left")
+        rospy.loginfo("Turn 2 completed!")
+        while not self.is_in_target_area(0, 1.5, 14, 15.5):
+            self.CarMove(1, 0)
+            rospy.sleep(0.05)
+            self.process_odom()
+        self.CarMove(0, 0)
+        rospy.loginfo("Straight 2 completed!")
         rospy.loginfo("Racecar reached, stop!")
 
+    def turn(self, target_x, target_y, direction):
+        while abs(self.calc_target_angle(target_x, target_y) - self.direction) > 20:
+            self.CarMove(1, 1 if direction == "left" else -1)
+            rospy.sleep(0.05)
+            self.process_odom()
+        while abs(self.calc_target_angle(target_x, target_y) - self.direction) > 5:
+            self.CarMove(0.3, 0.3 if direction == "left" else -0.3)
+            rospy.sleep(0.05)
+            self.process_odom()
+        while abs(self.calc_target_angle(target_x, target_y) - self.direction) > 1:
+            self.CarMove(0.1, 0.1 if direction == "left" else -0.1)
+            rospy.sleep(0.05)
+            self.process_odom()
+        self.CarMove(0, 0)
+
+    def calc_target_angle(self, x, y):
+        delta_x = x - self.pose.position.x
+        delta_y = y - self.pose.position.y
+        return math.atan2(delta_y, delta_x) / math.pi * 180
+
+    def is_in_target_area(self, x_min, x_max, y_min, y_max):
+        return (x_min < self.pose.position.x < x_max) and (y_min < self.pose.position.y < y_max)
+
+    def process_odom(self):
+        euler = euler_from_quaternion([self.pose.orientation.x, self.pose.orientation.y, self.pose.orientation.z, self.pose.orientation.w])
+        self.direction = euler[2] / math.pi * 180
+        # print("x: " + str("%.2f" % self.pose.position.x) + " y: " + str("%.2f" % self.pose.position.y) + " direction: " + str("%.2f" % self.direction))
+    
     def odom_callback(self, msg):
         self.pose = msg.pose.pose
 
